4305f3dffd72ac7382732d516514c87ea2e79118
mejdi14 <mejdiclubiste@gmail.com>
2022-06-23 16:54:01 +0100

    add banchmark to app

--- app/src/main/java/com/example/studio_ghibli_meets_compose/MainActivity.kt
+++ app/src/main/java/com/example/studio_ghibli_meets_compose/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("mejdi")
                 }
             }
         }
@@ -38,6 +38,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     StudioGhibliMeetsComposeTheme {
-        Greeting("Android")
+        Greeting("very nice")
     }
 }
