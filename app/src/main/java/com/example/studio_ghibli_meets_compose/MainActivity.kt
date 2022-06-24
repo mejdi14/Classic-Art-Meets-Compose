b98d3675533df650457bd1fe6ca0dc75aa0662c6
mejdi14 <mejdiclubiste@gmail.com>
2022-06-24 11:19:14 +0100

    playing with columns and rows

--- app/src/main/java/com/example/studio_ghibli_meets_compose/MainActivity.kt
+++ app/src/main/java/com/example/studio_ghibli_meets_compose/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.studio_ghibli_meets_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.studio_ghibli_meets_compose.ui.theme.StudioGhibliMeetsComposeTheme
@@ -22,7 +25,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("mejdi")
+                    Column(horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceBetween){
+                        Text("hello")
+                        Text("Mejdi")
+                    }
                 }
             }
         }
