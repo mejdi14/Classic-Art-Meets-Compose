d0af6fcf26252243115550adfaee08a2c1134460
mejdi14 <mejdiclubiste@gmail.com>
2022-07-26 14:57:47 +0100

    create the retrofit instance

--- core-network/src/main/java/com/example/core_network/network/retrofit/GhibliRetrofitNetwork.kt
+++ core-network/src/main/java/com/example/core_network/network/retrofit/GhibliRetrofitNetwork.kt
@@ -1,5 +1,6 @@
 package com.example.core_network.network.retrofit
 
+import com.example.core_network.BuildConfig
 import com.example.core_network.network.GhibliNetwork
 import com.example.core_network.network.model.Film
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
@@ -15,12 +16,26 @@ import javax.inject.Singleton
  * [Retrofit] backed [NiaNetwork]
  */
 @Singleton
-class RetrofitNiaNetwork @Inject constructor(
+class RetrofitGhibliNetwork @Inject constructor(
     networkJson: Json
 ) : GhibliNetwork {
     override suspend fun getTopics(ids: List<String>?): List<Film> {
         TODO("Not yet implemented")
     }
 
-
+    private val networkApi = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(
+            OkHttpClient.Builder()
+                .addInterceptor(
+                    // TODO: Decide logging logic
+                    HttpLoggingInterceptor().apply {
+                        setLevel(HttpLoggingInterceptor.Level.BODY)
+                    }
+                )
+                .build()
+        )
+        .addConverterFactory(networkJson.asConverterFactory("application/json".toMediaType()))
+        .build()
+        .create(RetrofitGhibliNetwork::class.java)
 }
