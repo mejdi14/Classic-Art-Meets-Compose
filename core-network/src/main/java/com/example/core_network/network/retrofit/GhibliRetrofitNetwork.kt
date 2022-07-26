10d09285b36d65d9b60581bf0eaf8e66b21a9c82
mejdi14 <mejdiclubiste@gmail.com>
2022-07-26 14:40:44 +0100

    declare the base url

--- core-network/src/main/java/com/example/core_network/network/retrofit/GhibliRetrofitNetwork.kt
+++ core-network/src/main/java/com/example/core_network/network/retrofit/GhibliRetrofitNetwork.kt
@@ -1,7 +1,13 @@
 package com.example.core_network.network.retrofit
 
 import com.example.core_network.network.GhibliNetwork
+import com.example.core_network.network.model.Film
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -12,38 +18,9 @@ import javax.inject.Singleton
 class RetrofitNiaNetwork @Inject constructor(
     networkJson: Json
 ) : GhibliNetwork {
+    override suspend fun getTopics(ids: List<String>?): List<Film> {
+        TODO("Not yet implemented")
+    }
 
-    private val networkApi = Retrofit.Builder()
-        .baseUrl(NiaBaseUrl)
-        .client(
-            OkHttpClient.Builder()
-                .addInterceptor(
-                    // TODO: Decide logging logic
-                    HttpLoggingInterceptor().apply {
-                        setLevel(HttpLoggingInterceptor.Level.BODY)
-                    }
-                )
-                .build()
-        )
-        .addConverterFactory(networkJson.asConverterFactory("application/json".toMediaType()))
-        .build()
-        .create(RetrofitNiaNetworkApi::class.java)
 
-    override suspend fun getTopics(ids: List<String>?): List<NetworkTopic> =
-        networkApi.getTopics(ids = ids).data
-
-    override suspend fun getAuthors(ids: List<String>?): List<NetworkAuthor> =
-        networkApi.getAuthors(ids = ids).data
-
-    override suspend fun getNewsResources(ids: List<String>?): List<NetworkNewsResource> =
-        networkApi.getNewsResources(ids = ids).data
-
-    override suspend fun getTopicChangeList(after: Int?): List<NetworkChangeList> =
-        networkApi.getTopicChangeList(after = after)
-
-    override suspend fun getAuthorChangeList(after: Int?): List<NetworkChangeList> =
-        networkApi.getAuthorsChangeList(after = after)
-
-    override suspend fun getNewsResourceChangeList(after: Int?): List<NetworkChangeList> =
-        networkApi.getNewsResourcesChangeList(after = after)
 }
