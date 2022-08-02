3806d235d3b5b66875f59116eb3506427dda8cb2
mejdi14 <mejdiclubiste@gmail.com>
2022-08-02 10:39:36 +0100

    save changes

--- core-network/src/main/java/com/example/corenetwork/network/retrofit/GhibliRetrofitApi.kt
+++ core-network/src/main/java/com/example/corenetwork/network/retrofit/GhibliRetrofitApi.kt
@@ -5,6 +5,5 @@ import retrofit2.http.GET
 
 interface GhibliRetrofitApi {
     @GET(value = "films")
-    suspend fun getFilms(
-    ): NetworkResponse<List<Film>>
+    suspend fun getFilms(): NetworkResponse<List<Film>>
 }
