51fed480d25a79f7421109b4d2140827e14685de
mejdi14 <mejdiclubiste@gmail.com>
2022-08-08 11:36:07 +0100

    yeah we added that one too

--- core-network/src/main/java/com/example/corenetwork/network/retrofit/ClassicArtRetrofitApi.kt
+++ core-network/src/main/java/com/example/corenetwork/network/retrofit/ClassicArtRetrofitApi.kt
@@ -1,9 +1,13 @@
 package com.example.corenetwork.network.retrofit
 
+import com.example.corenetwork.network.model.Agent
 import com.example.corenetwork.network.model.Artwork
 import retrofit2.http.GET
 
 interface ClassicArtRetrofitApi {
     @GET(value = "artworks")
-    suspend fun getArtwork(): NetworkResponse<Artwork>
+    suspend fun getALLArtworks(): NetworkResponse<Artwork>
+
+    @GET(value = "agents")
+    suspend fun getAllAgents(): NetworkResponse<Agent>
 }
