20c9b46fc39352adb50d64ebe78123032fca5ef2
mejdi14 <mejdiclubiste@gmail.com>
2022-08-09 17:16:00 +0100

    yeah it is fixed now

--- core-data/src/main/java/com/bk/coredata/PlaceUiState.kt
+++ core-data/src/main/java/com/bk/coredata/PlaceUiState.kt
@@ -8,6 +8,8 @@ sealed interface PlaceUiState {
      */
     object Loading : PlaceUiState
 
+    object Error : PlaceUiState
+
     /**
      * The feed is loaded with the given list of news resources.
      */
