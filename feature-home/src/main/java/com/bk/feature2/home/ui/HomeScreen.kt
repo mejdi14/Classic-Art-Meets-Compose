baff102d3b59a03e8597895bb7e44f14249c263a
mejdi14 <mejdiclubiste@gmail.com>
2022-08-17 16:32:23 +0100

    looks like we need the network too in the home screen

--- feature-home/src/main/java/com/bk/feature2/home/ui/HomeScreen.kt
+++ feature-home/src/main/java/com/bk/feature2/home/ui/HomeScreen.kt
@@ -1,6 +1,9 @@
 package com.bk.feature2.home.ui
 
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -8,6 +11,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
+import com.bk.core.data.DataUiState
 import com.bk.core.data.viewmodel.ArtworkViewModel
 
 @Composable
@@ -16,10 +20,18 @@ fun HomeRoute() {
 }
 
 @Composable
-fun HomeScreen(
-    viewModel: ArtworkViewModel = hiltViewModel()
-) {
+fun HomeScreen(viewModel: ArtworkViewModel = hiltViewModel()) {
     val navController = rememberNavController()
     val artworkUiState by viewModel.artworkUiState.collectAsState()
-    Text(text = "hello from home", modifier = Modifier.fillMaxSize())
+    LazyColumn(modifier = Modifier.fillMaxHeight()) {
+        items(
+            items = if (artworkUiState is DataUiState.Success<*>) {
+                @Suppress("UNCHECKED_CAST")
+                (placesUiState as DataUiState.Success<Artwork>).feed.data
+            } else arrayListOf(),
+            itemContent = { item ->
+                Text(text = item.title.toString())
+            }
+        )
+    }
 }
