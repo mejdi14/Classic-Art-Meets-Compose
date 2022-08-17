8238d3f0243de555df171ade81c23a2cf976110a
mejdi14 <mejdiclubiste@gmail.com>
2022-08-17 15:41:52 +0100

    artwork repository is ready

--- core-data/src/main/java/com/bk/core/data/repository/ArtworkRepositoryImp.kt
+++ core-data/src/main/java/com/bk/core/data/repository/ArtworkRepositoryImp.kt
@@ -1,14 +1,25 @@
 package com.bk.core.data.repository
 
+import com.bk.core.data.ClassicArtDispatchers
+import com.bk.core.data.Dispatcher
 import com.net.core.network.ClassicArtNetwork
 import com.net.core.network.model.Artwork
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class ArtworkRepositoryImp @Inject constructor(
+    @Dispatcher(ClassicArtDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
     private val network: ClassicArtNetwork
-) {
+) : ArtworkRepository {
 
     suspend fun getAllArtworks(): Artwork {
         return network.getArtwork()
     }
+
+    override fun getArtworkStream(): Flow<Result<Artwork>> = flow {
+        emit(Result.success(network.getArtwork()))
+    }.flowOn(ioDispatcher)
 }
