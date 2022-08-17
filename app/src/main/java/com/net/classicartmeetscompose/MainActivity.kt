d8856a00d16b816aaf9f87eebf0cbdad72d7df14
mejdi14 <mejdiclubiste@gmail.com>
2022-08-17 16:38:33 +0100

    clean up some extra lines

--- app/src/main/java/com/net/classicartmeetscompose/MainActivity.kt
+++ app/src/main/java/com/net/classicartmeetscompose/MainActivity.kt
@@ -42,39 +42,11 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        runBlocking {
-            launch {
-                /*  val list = placeViewModel.getAllPlaces()
-                  Log.d("TAG", "onCreate: $list")*/
-            }
-        }
         setContent {
-            /*ClassicArtComposeTheme {
-                Box(
-                    modifier = Modifier.fillMaxSize().background(color = Color(0xFFf7f9f5))
-                )
-                SimpleCoilImage(this)
-            }*/
             MainScreen()
         }
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Composable
-    private fun HomeScreen(viewModel: PlaceViewModel = hiltViewModel()) {
-        val placesUiState by viewModel.placesUiState.collectAsState()
-        LazyColumn(modifier = Modifier.fillMaxHeight()) {
-            items(
-                items = if (placesUiState is DataUiState.Success<*>) {
-                    @Suppress("UNCHECKED_CAST")
-                    (placesUiState as DataUiState.Success<Place>).feed.data
-                } else arrayListOf(),
-                itemContent = { item ->
-                    Text(text = item.title.toString())
-                }
-            )
-        }
-    }
 }
 
 @Composable
--- feature-home/src/main/java/com/bk/feature2/home/ui/HomeScreen.kt
+++ feature-home/src/main/java/com/bk/feature2/home/ui/HomeScreen.kt
@@ -1,7 +1,6 @@
 package com.bk.feature2.home.ui
 
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
@@ -10,9 +9,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.rememberNavController
 import com.bk.core.data.DataUiState
 import com.bk.core.data.viewmodel.ArtworkViewModel
+import com.net.core.network.model.Artwork
 
 @Composable
 fun HomeRoute() {
@@ -21,13 +20,12 @@ fun HomeRoute() {
 
 @Composable
 fun HomeScreen(viewModel: ArtworkViewModel = hiltViewModel()) {
-    val navController = rememberNavController()
     val artworkUiState by viewModel.artworkUiState.collectAsState()
     LazyColumn(modifier = Modifier.fillMaxHeight()) {
         items(
             items = if (artworkUiState is DataUiState.Success<*>) {
                 @Suppress("UNCHECKED_CAST")
-                (placesUiState as DataUiState.Success<Artwork>).feed.data
+                (artworkUiState as DataUiState.Success<Artwork>).feed.data
             } else arrayListOf(),
             itemContent = { item ->
                 Text(text = item.title.toString())
