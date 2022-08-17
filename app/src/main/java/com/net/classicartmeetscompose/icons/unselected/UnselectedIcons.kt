ce2cca433aeacde7350da91aa404f6161516c26d
mejdi14 <mejdiclubiste@gmail.com>
2022-08-17 11:41:36 +0100

    a bit more frustrating but that is because i am on drugs

--- app/src/main/java/com/net/classicartmeetscompose/icons/unselected/UnselectedIcons.kt
+++ app/src/main/java/com/net/classicartmeetscompose/icons/unselected/UnselectedIcons.kt
@@ -1,96 +1,7 @@
 package com.net.classicartmeetscompose.icons.unselected
 
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Bell
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Bookmark
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Calendar
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Camera
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Clock
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Comment
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Comments
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Compass
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Copy
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Coupon
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Desktop
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Discount
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Download
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Envelope
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Eye
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.File
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Film
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Filter
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Flag
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Folder
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Globe
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Headphones
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Heart
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Home
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Image
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Inbox
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Link
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Lock
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Map
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Menu
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Microphone
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Mobile
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Package
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Paperclip
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Pen
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Phone
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Search
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Send
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Settings
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Shield
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Star
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Stopwatch
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Store
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Tablet
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Tag
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Thumbtack
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Trash
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Unlock
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Upload
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.User
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Users
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.Video
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Arrow-down`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Arrow-left`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Arrow-right`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Arrow-up`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Bell-slash`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Chart-pie`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Check-circle`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Chevron-circle-down`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Chevron-circle-left`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Chevron-circle-right`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Chevron-circle-up`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Chevron-down`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Chevron-left`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Chevron-right`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Chevron-up`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Comment-dots`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Credit-card`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Exclamation-circle`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Eye-slash`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Info-circle`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Log-in`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Log-out`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Map-marker`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Map-pin`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Microphone-slash`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Minus-square`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`More-h-circle`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`More-v-circle`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Plus-square`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Question-circle`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Shopping-basket`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Sliders-h`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Sort-ascending`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Sort-descending`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Times-square`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Users-three`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Video-slash`
-import com.net.classicartmeetscompose.icons.unselected.myiconpack.`Volume-up`
+import com.net.classicartmeetscompose.icons.unselected.myiconpack.*
 import kotlin.collections.List as ____KtList
 
 public object UnselectedIcons
@@ -98,22 +9,62 @@ public object UnselectedIcons
 private var allIcons: ____KtList<ImageVector>? = null
 
 public val UnselectedIcons.AllIcons: ____KtList<ImageVector>
-  get() {
-    if (allIcons != null) {
-      return allIcons!!
+    get() {
+        if (allIcons != null) {
+            return allIcons!!
+        }
+        allIcons = listOf(
+            Bell,
+            Bookmark,
+            Calendar,
+            Camera,
+            Comment,
+            Comments,
+            Compass,
+            Copy,
+            Coupon,
+            Desktop,
+            Discount,
+            Download,
+            Envelope,
+            Eye,
+            File,
+            Film,
+            Filter,
+            Flag,
+            Folder,
+            Globe,
+            Headphones,
+            Heart,
+            Home,
+            Image,
+            Inbox,
+            Link,
+            Lock,
+            Map,
+            Menu,
+            Microphone,
+            Mobile,
+            Package,
+            Paperclip,
+            Pen,
+            Phone,
+            Search,
+            Send,
+            Settings,
+            Shield,
+            Star,
+            Stopwatch,
+            Store,
+            Tablet,
+            Tag,
+            Thumbtack,
+            Trash,
+            Unlock,
+            Upload,
+            User,
+            Users,
+            Video,
+        )
+        return allIcons!!
     }
-    allIcons= listOf(`Arrow-down`, `Arrow-left`, `Arrow-right`, `Arrow-up`, `Bell-slash`, Bell,
-        Bookmark, Calendar, Camera, `Chart-pie`, `Check-circle`, `Chevron-circle-down`,
-        `Chevron-circle-left`, `Chevron-circle-right`, `Chevron-circle-up`, `Chevron-down`,
-        `Chevron-left`, `Chevron-right`, `Chevron-up`, Clock, `Comment-dots`, Comment, Comments,
-        Compass, Copy, Coupon, `Credit-card`, Desktop, Discount, Download, Envelope,
-        `Exclamation-circle`, `Eye-slash`, Eye, File, Film, Filter, Flag, Folder, Globe, Headphones,
-        Heart, Home, Image, Inbox, `Info-circle`, Link, Lock, `Log-in`, `Log-out`, `Map-marker`,
-        `Map-pin`, Map, Menu, `Microphone-slash`, Microphone, `Minus-square`, Mobile,
-        `More-h-circle`, `More-v-circle`, Package, Paperclip, Pen, Phone, `Plus-square`,
-        `Question-circle`, Search, Send, Settings, Shield, `Shopping-basket`, `Sliders-h`,
-        `Sort-ascending`, `Sort-descending`, Star, Stopwatch, Store, Tablet, Tag, Thumbtack,
-        `Times-square`, Trash, Unlock, Upload, User, `Users-three`, Users, `Video-slash`, Video,
-        `Volume-up`)
-    return allIcons!!
-  }
